704aa86bd3b01bd2b965330179163f49ab356711
gguilhermelopes <guilhermelopes275@gmail.com>
2024-01-09 21:12:47 -0300

    feat: products page

--- src/app/main/projects/page.tsx
+++ src/app/main/projects/page.tsx
@@ -1,7 +1,8 @@
+import ProjectsPage from "@/components/projects";
 import React from "react";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  return <ProjectsPage />;
 };
 
 export default Projects;
---
+++ src/components/projects/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AnimateLeftIn from "../UI/AnimateLeftIn";
+
+const ProjectsPage = () => {
+  return (
+    <AnimateLeftIn>
+      <main>Page</main>
+    </AnimateLeftIn>
+  );
+};
+
+export default ProjectsPage;
