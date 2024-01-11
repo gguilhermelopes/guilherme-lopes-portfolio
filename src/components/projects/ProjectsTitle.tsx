3ed707c7a111f56d91560e3242f65c160616c3fd
gguilhermelopes <guilhermelopes275@gmail.com>
2024-01-10 22:32:50 -0300

    feat: set ProjectsPage to components

---
+++ src/components/projects/ProjectsTitle.tsx
@@ -0,0 +1,11 @@
+import TextDetails from "../UI/TextDetails";
+
+const ProjectsTitle = () => {
+  return (
+    <h1 className="text-3xl font-bold">
+      Conheça alguns dos meus <TextDetails>projetos</TextDetails>
+    </h1>
+  );
+};
+
+export default ProjectsTitle;
