7bafbdc5910313ac02e79888c7612b158f1a2ae7
gguilhermelopes <guilhermelopes275@gmail.com>
2024-02-21 23:08:47 -0300

    feat: change profile picture bg

--- src/components/main/ProfilePhoto.tsx
+++ src/components/main/ProfilePhoto.tsx
@@ -5,9 +5,9 @@ const ProfilePhoto = () => {
   return (
     <Image
       className="h-[200px] w-[150px] sm:h-[300px] sm:w-[225px] md:w-[300px] md:h-[400px] object-cover rounded-md"
-      width={300}
-      height={400}
-      src="/index.jpg"
+      width={600}
+      height={800}
+      src="/index.png"
       alt="Guilherme Lopes"
       priority
     />
